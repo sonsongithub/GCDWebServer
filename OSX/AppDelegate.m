50a2d4ea74e24afee524f0f72483381ca670af67
Yuichi Yoshida <yoshida.yuichi@gmail.com>
2013-03-27 08:10:08 +0900

    Added simple sample codes. version OSX blocks main thread…? Why…?

--- OSX/AppDelegate.m
+++ OSX/AppDelegate.m
@@ -8,11 +8,19 @@
 
 #import "AppDelegate.h"
 
+#import "GCDWebServer.h"
+
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-	// Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// test start
+	GCDWebServer* webServer = [[GCDWebServer alloc] init];
+	[webServer addDefaultHandlerForMethod:@"GET"
+							 requestClass:[GCDWebServerRequest class]
+							 processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
+								 return [GCDWebServerDataResponse responseWithHTML:@"<html><body><p>Hello World</p></body></html>"];
+							 }];
+	[webServer runWithPort:8085];
 }
 
 @end
